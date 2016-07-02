589e7f5ab5eaba72ff7e6b071a78ac4675d53428
Henry Vu <HenrytheVU@gmail.com>
2016-07-02 17:42:52 +0200

    first commit

---
+++ src/main/java/de/fhws/FrontendRouterApplication.java
@@ -0,0 +1,12 @@
+package de.fhws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendRouterApplication.class, args);
+	}
+}
